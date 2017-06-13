0435f3aed7b3c702746c2fa6e5ceff6fc002665d
tmt514 <math.tmt514@gmail.com>
2017-06-13 10:58:59 -0400

    modify install scripts

--- install.py
+++ install.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 import shutil
+import os
+from stat import S_IRWXU, S_IXGRP, S_IXOTH
 
-shutil.file("cf-fetch.py", "~/.local/bin/cf-fetch")
-shutil.file("cf-test.py", "~/.local/bin/cf-test")
-shutil.file("code_submit.py", "~/.local/bin/cf-submit")
+homedir = os.path.expanduser("~")
+install_files = [("cf-fetch.py", "cf-fetch"),
+    ("cf-test.py", "cf-test"),
+    ("code_submit.py", "cf-submit")]
+for src, dst in install_files:
+  mdst = os.path.join(homedir, ".local", "bin", dst)
+  shutil.copyfile(src, mdst)
+  os.chmod(mdst, S_IRWXU | S_IXGRP | S_IXOTH)
